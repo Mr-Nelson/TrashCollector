24829a36b14a24cebd490f9901ac46ebdb78b32a
jschenkelberg <jmschenkelberg@gmail.com>
2021-07-05 13:08:43 -0500

    onetime pickup method is functional

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -80,13 +80,19 @@ def create_edit_pickup(request):
 
 
 def onetime_pickup(request):
+    user = request.user
+    print(user.id)
+    new_onetime_pickup = Customer.objects.get()
+    context = {
+            'new_onetime_pickup': new_onetime_pickup
+    }
     if request.method == 'POST':
-        onetime_pickup = request.POST.get('onetime_pickup')
-        new_onetime_pickup = Customer(onetime_pickup=onetime_pickup)
+        new_onetime_pickup.onetime_pickup = request.POST.get('onetime_pickup')
+        # new_onetime_pickup = Customer(onetime_pickup=onetime_pickup)
         new_onetime_pickup.save()
         return HttpResponseRedirect(reverse('customers:index'))
     else:
-        return render(request, "customers/onetime_pickup.html")
+        return render(request, "customers/onetime_pickup.html", context)
         pass
     #       create format for onetime_pickup object
 def pickup_suspension(request):
